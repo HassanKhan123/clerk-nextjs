80024efca2d737946eb2e7b3fd1463da8f7ec759
HassanKhan123 <hk2602809@gmail.com>
2023-07-25 13:05:56 +0500

    added: user profile page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,7 +1,10 @@
+import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { userId } = auth();
+
   return (
     <>
       <nav className='bg-blue-700 py-4 px-6 flex items-center justify-between mb-5'>
@@ -13,13 +16,35 @@ const Header = () => {
           </Link>
         </div>
 
-        <div className='text-white'>
-          <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'>
-            Sign In
-          </Link>
-          <Link href='sign-up' className='text-gray-300 hover:text-white mr-4'>
-            Sign Up
-          </Link>
+        <div className='text-white flex items-center'>
+          {!userId && (
+            <>
+              <Link
+                href='sign-in'
+                className='text-gray-300 hover:text-white mr-4'
+              >
+                Sign In
+              </Link>
+              <Link
+                href='sign-up'
+                className='text-gray-300 hover:text-white mr-4'
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
+
+          {userId && (
+            <Link
+              href='profile'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Profile
+            </Link>
+          )}
+          <div className='ml-auto'>
+            <UserButton afterSignOutUrl='/' />
+          </div>
         </div>
       </nav>
     </>
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className='text-2xl font-bold mb-5'>Weclome</h1>
+      <p className='mb-5'>
+        This is a demo site for the Next.js & Clerk App. Go ahead amd signup or
+        sign in!
+      </p>
     </>
   );
 }
